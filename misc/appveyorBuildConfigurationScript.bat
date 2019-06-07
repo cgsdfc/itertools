a4176ce6f92d1572f75b3d0b06e7a340e838617f
cgsdfc <cgsdfc@126.com>
2019-06-07 17:56:55 +0800

    Remove call to generateSingleHeader.

--- misc/appveyorBuildConfigurationScript.bat
+++ misc/appveyorBuildConfigurationScript.bat
@@ -12,9 +12,7 @@ if "%CONFIGURATION%"=="Debug" (
     cmake --build build-misc || exit /b !ERRORLEVEL!
     cmake -H. -BBuild -A%PLATFORM% -DUSE_WMAIN=%wmain% -DMEMORYCHECK_COMMAND=build-misc\Debug\CoverageHelper.exe -DMEMORYCHECK_COMMAND_OPTIONS=--sep-- -DMEMORYCHECK_TYPE=Valgrind || exit /b !ERRORLEVEL! || exit /b !ERRORLEVEL!
   ) else (
-    @REM # We know that coverage is 0
-    python scripts\generateSingleHeader.py || exit /b !ERRORLEVEL!
-    cmake -H. -BBuild -A%PLATFORM% -DUSE_WMAIN=%wmain% -DCATCH_BUILD_EXAMPLES=%examples% -DCATCH_BUILD_EXTRA_TESTS=%examples% || exit /b !ERRORLEVEL!
+    cmake -H. -BBuild -A%PLATFORM% -DUSE_WMAIN=%wmain% -DITERTOOLS_BUILD_EXAMPLES=%examples% -DITERTOOLS_BUILD_EXTRA_TESTS=%examples% || exit /b !ERRORLEVEL!
   )
 )
 if "%CONFIGURATION%"=="Release" (
